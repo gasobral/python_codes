d3c57ecfaf32542469bd3e82ca2be5cd6a40551e
gasobral <gasobral@gmail.com>
2016-06-01 16:00:01 -0300

    included help and load functions

--- ref_utils.py
+++ ref_utils.py
@@ -9,12 +9,12 @@
 import sys
 
 # Functions - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# -- > read a command from keyboard - - - - - - - - - - - - - - - - - - - - - #
 def read_cmd():
     """
     Function      : read_cmd
     Arguments     : none
-    Description   :
-    Reads a command from command from keyboard
+    Description   : Reads a command from command from keyboard
     Return Values :
     pointer to function \t if it's a valid command it's the address to it
     None \t if Ctrl-D or Ctrl-C is typed
@@ -32,13 +32,42 @@ def read_cmd():
     return know_cmd[user_cmd]
 
 
+# --> diplayes the help for each command - - - - - - - - - - - - - - - - - - -#
+def _help() :
+    """
+    Function      : help
+    Arguments     : none
+    Description   : Prints the description of all available commands for this \
+                    program.
+    Return Values :
+    This function has no return values
+    """
+
+    for cmd in know_cmd.values() :
+        print(cmd.__doc__)
+
+
+# --> loads data from a bibtex file - - - - - - - - - - - - - - - - - - - - - #
+def _load(file_name) :
+    """
+    Function      : load
+    Arguments     : file_name
+    Description   : This function loads the abritutes of all references in a\
+                    bibtex file and setup them to add notes to it.
+    Return Values :
+    This function has no return values
+    """
+
+    pass
+
+
+# --> main function of this script - - - - - - - - - - - - - - - - - - - - - -#
 def main(args) :
     """
     Function      : main
     Arguments     : sys.argv
-    Description   :
-    This program loads bibtex files, create notes to it and remove unused\n\
-    references.
+    Description   : This program loads bibtex files, create notes to it and\
+                    remove unused references.
     Return Values :
     0 \t if executed without errors
     1 \t if an error has occurred
@@ -56,13 +85,18 @@ def main(args) :
 
         if user_cmd == None :
             exit_status = True
-
+            
+        else :
+            user_cmd()
+            
     return 0
 
 
 # Data Structures - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-# { function_name = key : function_pointer = value } #
-know_cmd = {'main' : main}
+# know_cmd is a dic which holds the known commands user can input, and it has #
+# the structure below :                                                       #
+# { function_name = key : function_pointer = value }                          #
+know_cmd = {'main' : main, 'help' : _help, 'load' : _load}
 
 # Main Script - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 return_code = 0
