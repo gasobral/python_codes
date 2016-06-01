dbebcd7ab46b67385ad8d5bcdd1e61d49a4af6a4
gagsobral <gagsobral@inf.ufpr.br>
2016-06-01 10:47:03 -0300

    Added some doc strings and created read_cmd function

--- ref_utils.py
+++ ref_utils.py
@@ -8,32 +8,61 @@
 # Module Import - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 import sys
 
-# Data Structures - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-# { function_name = key : function_pointer = value } #
-know_cmd = {}
-
 # Functions - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 def read_cmd():
-    """Reads a command from command from stdin.\n\
-If it's a valid command then return the respective function.\n\
-Otherwise returns None.
-"""
-    pass
+    """
+    Function      : read_cmd
+    Arguments     : none
+    Description   :
+    Reads a command from command from keyboard
+    Return Values :
+    pointer to function \t if it's a valid command it's the address to it
+    None \t if Ctrl-D or Ctrl-C is typed
+    """
+
+    user_cmd = ""
+
+    while (user_cmd in know_cmd.keys()) == False :
+        try :
+            user_cmd = input()
+
+        except (KeyboardInterrupt, EOFError) :
+            return None
+
+    return know_cmd[user_cmd]
 
 
 def main(args) :
-    """This program loads bibtex files, create notes to it and remove unused\n\
-references."""
+    """
+    Function      : main
+    Arguments     : sys.argv
+    Description   :
+    This program loads bibtex files, create notes to it and remove unused\n\
+    references.
+    Return Values :
+    0 \t if executed without errors
+    1 \t if an error has occurred
+    """
 
     print(main.__doc__)
-    exit_status = False # variable that show if the program ends or not #
 
-    # main loop, read files, make notes, etc #
-    while exit_status = False :
-        user_cmd = input()
+    # --> variable initialization #
+    exit_status = False # used to control if leave the program or not #
+    user_cmd = ""       # it has the command read from keyboard       #
+
+    # --> main loop of the script, which commands are read and executed #
+    while exit_status == False :
+        user_cmd = read_cmd()
+
+        if user_cmd == None :
+            exit_status = True
 
     return 0
 
+
+# Data Structures - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# { function_name = key : function_pointer = value } #
+know_cmd = {'main' : main}
 
 # Main Script - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 return_code = 0
