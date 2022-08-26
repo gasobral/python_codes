00b600a8023596245f0209a2110179c8d8ecee07
Gabriel Sobral <gasobral@gmail.com>
2022-08-26 17:30:33 -0300

    Adicionei os exercícios de IPE

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# Description - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-# This file has a list of all python scripts created.                         #
-
-__all__ = ["ref_utils"]
