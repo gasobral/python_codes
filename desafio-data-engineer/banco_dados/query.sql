676ea75f758ef88837d3f0eea597d8bb0c6d4e52
gasobral <gasobral@gmail.com>
2025-05-19 17:23:35 -0300

    Movido para outro diretório

--- desafio-data-engineer/banco_dados/query.sql
+++
@@ -1,7 +0,0 @@
-select oi.freight_value, gs.geolocation_lat, gs.geolocation_lng, gc.geolocation_lat, gc.geolocation_lng
-from order_items oi, sellers s, geolocation gs, orders o, customers c, geolocation gc
-where oi.seller_id = s.seller_id and
-s.seller_zip_code_prefix = gs.geolocation_zip_code_prefix and
-o.order_id = oi.order_id and
-o.customer_id = c.customer_id and
-c.customer_zip_code_prefix = gc.geolocation_zip_code_prefix
